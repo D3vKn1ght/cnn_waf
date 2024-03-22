b89d59587f8ffe821a249b20269deede3bb14e12
trung501 <gotrungogle@gmail.com>
2024-03-22 07:11:59 +0000

    update

--- api_server.py
+++ api_server.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, HTTPException, Body
 from pydantic import BaseModel
 import uvicorn
+import random
+import logging
 from detector import Detector
 
 app = FastAPI()
@@ -8,6 +10,13 @@ app = FastAPI()
 class Item(BaseModel):
     method: str="GET"
     uri: str="/tienda1/example"
+
+def format_uri(uri):
+    space_char = ["%20", "+"]
+    random_space = random.choice(space_char)
+    uri = uri.replace(" ", random_space)
+    return uri
+    
 
 @app.get("/")
 async def read_root():
@@ -19,9 +28,11 @@ async def detect_url(item: Item):
     try:
         method = item.method.upper().strip()
         uri = item.uri.strip()
+        uri = format_uri(uri)
         payload = method + " " + uri
         detector = Detector()
         label_pred = detector.predict_url(payload)
+        logging.warning(f"method: {method}, uri: {uri}, label: {label_pred}")
         return {"method": method, "uri": uri, "label": label_pred}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
